da1c694a0cee36cf1a7053cf8ffc319879b1c29d
hhuynh-nova <80795253+hhuynh-nova@users.noreply.github.com>
2021-11-09 16:53:06 -0500

    Update ice-cream-schema.sql

--- ice-cream-schema.sql
+++ ice-cream-schema.sql
@@ -1,28 +1,24 @@
 DROP TABLE employee CASCADE CONSTRAINTS;
 CREATE TABLE employee (
-    f_name           varchar2(15) not null, 
-    l_name           varchar2(15) not null,
+    emp_ID          number(3) not null,
+    f_name          varchar2(15) not null, 
+    l_name          varchar2(15) not null,
     ssn             char(9),
     bdate           date not null,
     address         varchar2(50) not null,
     phone           number(10),
     sex             char,
     salary          number(10,2),
-    super_name        char(9),
-    emp_ID          number(3) not null,
+    super_name      char(9),
     primary key (emp_ID)
 );
 
 DROP TABLE customer CASCADE CONSTRAINTS;
 CREATE TABLE customer (
-    f_name           varchar2(15) not null, 
-    l_name           varchar2(15),
     cust_ID         number(5) not null,
+    f_name          varchar2(15) not null, 
     email           varchar2(50) not null,
-    membership      char(1),
-    phone           number(10),
-    bdate           date,
-    address         varchar2(50),
+    membership      varchar(100),
     primary key (cust_ID)
 );
 
@@ -31,22 +27,20 @@ CREATE TABLE cust_order (
     orderno         number(5) not null,
     cust_ID         number(5) not null,
     emp_ID          number(3) not null,
-    prod_num         number(5) not null,
-    flavor          varchar(50),
-    contain         varchar(20),
-    topping         varchar(20),
-    total           decimal(5,2) not null,
+--    prod_num         number(5) not null,
+    product         varchar(100),
+    price           decimal(5,2) not null,
     foreign key (cust_ID) references customer(cust_ID),
-    foreign key (emp_ID) references employee(emp_ID),
-    foreign key (prod_num) references product(prod_num)
+    foreign key (emp_ID) references employee(emp_ID)
+--    foreign key (prod_num) references product(prod_num)
 );
 
-DROP TABLE product CASCADE CONSTRAINTS;
-CREATE TABLE product (
-    prod_num         number(5) not null,
-    prod_name       varchar(50) not null,
-    quantity        number(5),
-    price           decimal(5,2) not null,
-    vend_name       varchar(20),
-    primary key (prod_num)
-);
+--DROP TABLE product CASCADE CONSTRAINTS;
+--CREATE TABLE product (
+--    prod_num         number(5) not null,
+--    prod_name       varchar(50) not null,
+--    quantity        number(5),
+--    price           decimal(5,2) not null,
+--    vend_name       varchar(20),
+--    primary key (prod_num)
+--);
